1e326749d262bc8ade31b0d93c15320f18399315
Galih Setyawan <galih.kamvax032@gmail.com>
2023-10-13 21:48:59 +0700

    add login usecase

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ConfirmationPage from './pages/confirmationPage';
 import LandingPage from './pages/landingPage';
 import RegisterPage from './pages/registerPage';
 import UseCasePage from './pages/useCasePage';
+import LoginPage from './pages/useCasePage/component/loginPage';
 import SuccessPage from './pages/useCasePage/component/successPage';
 import "./styles/variables.scss";
 
@@ -13,7 +14,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/confirmation" element={<ConfirmationPage />} />
-        <Route path="/usecase" element={<UseCasePage />} />
+        <Route path="/usecase" element={<LoginPage />} />
+        <Route path="/usecase-form" element={<UseCasePage />} />
         <Route path="/success-page" element={<SuccessPage />} />
       </Routes>
     </Router>
--- src/pages/useCasePage/component/header/index.tsx
+++ src/pages/useCasePage/component/header/index.tsx
@@ -3,18 +3,21 @@ import mockup from "../../../../assets/images/people-img.png";
 import "./styles.scss";
 
 const HeaderUsecase = () => {
+    const email = localStorage.getItem('userEmail')
     return (
         <div className="header-wp">
             <div className="logo-wp">
                 <img src={logo} height="50" />
                 <p className="title">Form Pengambilan Usecase</p>
             </div>
-            <div className="account-wp">
+            {email && (
+                <div className="account-wp">
                 <div className="img-wp">
                     <img src={mockup} className="img-account" />
                 </div>
                 <p className="title-account">Mubarak Al-Fatih</p>
             </div>
+            )}
         </div>
     )
 }
